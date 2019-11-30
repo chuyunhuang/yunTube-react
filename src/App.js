c9f0c4fdfe2a379d00cb162b313e6018d73e27d8
Chuyun Huang <chuyunhuang@pursuit.org>
2019-11-30 18:13:50 -0500

    added react-router-dom and used browserRouter in index.js and fixed app.js by adding the correct routing for Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 //Components
 import { Home } from './Containers/Home/Home';
 import { AppLayout } from './Components/AppLayout/AppLayout';
+import { Watch } from './Containers/Watch/Watch';
 
 function App() {
   return (
     <AppLayout >
-      <Home />
+      <Switch>
+        <Route path="/watch" component={Watch} />
+        <Route path="/" component={Home} />
+      </Switch>
     </AppLayout>
   );
 }
