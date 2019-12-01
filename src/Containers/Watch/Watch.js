ea4e37af2c9c3b42b42c9e0f022f4636d1ab4526
Chuyun Huang <chuyunhuang@pursuit.org>
2019-12-01 18:34:43 -0500

    added grid layout to the entire Watch container

--- src/Containers/Watch/Watch.js
+++ src/Containers/Watch/Watch.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Watch.scss';
-// import { Video } from '../../Components/Video/Video';
-// import { VideoPreview } from '../../Components/VideoPreview/VideoPreview';
+import { Video } from '../../Components/Video/Video';
 import { RelatedVideos } from '../../Components/RelatedVideos/RelatedVideos';
 
 export class Watch extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <RelatedVideos />
-      </React.Fragment>
+      <div className="watch-grid">
+        <Video className="video" id="-7fuHEEmEjs" />
+        <div className="metadata" style={{ width: '100%', height: '100px', background: '#F91112' }}>Metadata</div>
+        <div className="video-info-box" style={{ width: '100%', height: '100px', background: 'orange' }}>Info</div>
+        <div className="comments" style={{ width: '100%', height: '100px', background: 'green' }}>Comments</div>
+        <RelatedVideos className="relatedVideos" />
+      </div>
     )
   }
 }
