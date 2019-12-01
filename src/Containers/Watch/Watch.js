e347f8c35c540f9f9f80b1ced6a56062d5e3d7da
Chuyun Huang <chuyunhuang@pursuit.org>
2019-12-01 18:11:01 -0500

    added RelatedVideos component and NextUpVideo component UI

--- src/Containers/Watch/Watch.js
+++ src/Containers/Watch/Watch.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './Watch.scss';
 // import { Video } from '../../Components/Video/Video';
-import { VideoPreview } from '../../Components/VideoPreview/VideoPreview';
+// import { VideoPreview } from '../../Components/VideoPreview/VideoPreview';
+import { RelatedVideos } from '../../Components/RelatedVideos/RelatedVideos';
 
 export class Watch extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {/* <div style={{ maxWidth: '80%' }}>
-        <Video id='-7fuHEEmEjs' />
-      </div> */}
-        <VideoPreview horizontal={true} />
-        <VideoPreview />
+        <RelatedVideos />
       </React.Fragment>
     )
   }
