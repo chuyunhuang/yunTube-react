af1ecb9f56e2050bec93cc2a20b76e370a846692
Chuyun Huang <chuyunhuang@pursuit.org>
2019-12-01 17:16:06 -0500

    added Video component and styling

--- src/Containers/Watch/Watch.js
+++ src/Containers/Watch/Watch.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Watch.scss';
+import { Video } from '../../Components/Video/Video';
 
 export class Watch extends React.Component {
   render() {
     return (
-      <div>Watch Component</div>
+      <div style={{ maxWidth: '80%' }}>
+        <Video id='-7fuHEEmEjs' />
+      </div>
     )
   }
 }
