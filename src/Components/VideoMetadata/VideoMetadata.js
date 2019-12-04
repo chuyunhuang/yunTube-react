508b9bb5f5687c6bfa98318ffc76346dbedf4894
Chuyun Huang <chuyunhuang@pursuit.org>
2019-12-03 22:10:42 -0500

    added VideoMetadata and Rating components for Watch

--- src/Components/VideoMetadata/VideoMetadata.js
+++ src/Components/VideoMetadata/VideoMetadata.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Divider } from 'semantic-ui-react';
+import { Divider, Button, Icon } from 'semantic-ui-react';
 import './VideoMetadata.scss';
+import { Rating } from '../Rating/Rating';
 
 export function VideoMetadata(props) {
   return (
     <div className="video-metadata">
       <h3>Video Title</h3>
       <div className="video-stats">
-        <span>views</span>
+        <span> 1,000 views</span>
+        <div className="video-action">
+          <Rating likeCount={20} dislikeCount={1} />
+          <Button basic icon labelposition="left" >
+            <Icon name="share" />
+            Share
+          </Button>
+          <Button basic icon>
+            <Icon name="add circle" />
+          </Button>
+        </div>
       </div>
-      <div>Sth</div>
       <Divider />
     </div>
   )
--- src/Containers/Watch/Watch.js
+++ src/Containers/Watch/Watch.js
@@ -9,7 +9,7 @@ export class Watch extends React.Component {
     return (
       <div className="watch-grid">
         <Video className="video" id="-7fuHEEmEjs" />
-        <VideoMetadata className="metadata" viewCount={1000} />
+        <VideoMetadata className="video-metadata" viewCount={1000} />
         <div className="video-info-box" style={{ width: '100%', height: '100px', background: 'orange' }}>Info</div>
         <div className="comments" style={{ width: '100%', height: '100px', background: 'green' }}>Comments</div>
         <RelatedVideos className="relatedVideos" />
